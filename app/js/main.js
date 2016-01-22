19efb73b59fff5d757aec0f4a90680784b2614fe
Noobko <YaroslavKryvda@gmail.com>
2016-01-22 14:08:40 +0200

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function() {
+	
+	// Проверка наличия JS, jQuery.
+	console.log("js works");
+	if($) {
+		console.log("jQuery works");
+	}
+	
+	// toggle функция открытия/закрытия меню.
+	var openCloseMenu = function() {
+		$(".mnu-button-wrap").click(function() {
+			$(".arrow-up").toggleClass("hide");
+			$(".nav-mnu-head").toggleClass("hide");
+		});
+	};
+
+	openCloseMenu();
+
+	closeSucsessWindow();
+
+	addProject();
+
+	addProjectPopup();
+
+	openCloseAuthPopup();
+});	
