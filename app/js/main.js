0e5253f3532c0e3cf7ccdefc38b2ce8e8ac36420
Noobko <YaroslavKryvda@gmail.com>
2016-02-03 15:45:21 +0200

    add popup elements, fix bugs

--- app/js/main.js
+++ app/js/main.js
@@ -5,7 +5,53 @@ $(document).ready(function() {
 	if($) {
 		console.log("jQuery works");
 	}
-	
+	// Функция открывает/закрывает окно Авторизации.
+	var openCloseLoginPopup = function() {
+		$(".lock-box").click(function() { // Нажатие на замочек.
+			$(this).toggleClass("lock-box-active");
+			if ($(".login-popup").hasClass("hide")) {
+				$(".login-popup").removeClass("hide");
+				$(".login-form").removeClass("hide");
+				$(".header").addClass("unvis");
+				$(".container").addClass("unvis");
+				$(".footer").addClass("unvis");
+			} else {
+				$(".login-popup").addClass("hide");
+				$(".login-form").addClass("hide");
+				$(".header").removeClass("unvis");
+				$(".container").removeClass("unvis");
+				$(".footer").removeClass("unvis");
+			}
+		});
+
+		$(".lock-text").click(function() { // Нажатие на текст рядом с замочком.
+			$(".lock-box").toggleClass("lock-box-active");
+			if ($(".login-popup").hasClass("hide")) {
+				$(".login-popup").removeClass("hide");
+				$(".login-form").removeClass("hide");
+				$(".header").addClass("unvis");
+				$(".container").addClass("unvis");
+				$(".footer").addClass("unvis");
+			} else {
+				$(".login-popup").addClass("hide");
+				$(".login-form").addClass("hide");
+				$(".header").removeClass("unvis");
+				$(".container").removeClass("unvis");
+				$(".footer").removeClass("unvis");
+			}
+		});
+
+		$(".login-form-window-close-x").click(function() { // Нажатие на крестик, закрывающий окошко.
+			$(".lock-box").removeClass("lock-box-active");
+			$(".login-popup").addClass("hide");
+			$(".login-form").addClass("hide");
+			$(".header").removeClass("unvis");
+			$(".container").removeClass("unvis");
+			$(".footer").removeClass("unvis");
+		});
+	};
+	openCloseLoginPopup();
+
 	// toggle функция открытия/закрытия меню.
 	var openCloseMenu = function() {
 		$(".mnu-button-wrap").click(function() {
@@ -15,5 +61,29 @@ $(document).ready(function() {
 	};
 
 	openCloseMenu();
+
+	// Функция, показывает окно с сообщением от успешном добавлении проекта после сабмита формы.
+	var addProject = function() {
+		$(".add-proj-button-input").on("submit", function() {
+			$(".project-window").addClass("hide");
+			$(".sucsess-window").removeClass("hide");
+		});
+	};
+	addProject();
+
+	// Функция открывает/закрывает окно добавления работы портфолио.
+	var addProjectPopup = function() {
+		$(".add-project-box").click(function() {
+			$(".project-popup").removeClass("hide");
+			$(".project-window").removeClass("hide");
+		});
+		$(".window-close-box").click(function() {
+			$(".project-popup").addClass("hide");
+			$(".project-window").addClass("hide");
+		});
+	};
+
+
+	addProjectPopup();
 
 });	
